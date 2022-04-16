c37421f2b9fd1592392cd8baa2fdcd6e9037f5ac
SGORTSOKO <99719292+SGORTSOKO@users.noreply.github.com>
2022-04-16 08:19:10 +0500

    add phone

--- 2 pract/demo/src/main/java/com/example/demo/controller/MainController.java
+++ 2 pract/demo/src/main/java/com/example/demo/controller/MainController.java
@@ -6,6 +6,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import com.example.demo.Phone;
+
+
 @Controller
 		public class MainController {
 			
@@ -18,10 +27,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 			//обработка запросов к коренному или index методом get
 			//и передает модель методу
 			public String index(Model model) {
-				//добавить в модель атрибут
-				model.addAttribute("message", message);
-				//чтение будет их ресурсов
-				return "index";
-				//возврат имени файла - html
-			}
+	    	List<Phone> lp = new ArrayList<>();
+	    	lp.add(new Phone("Абросимов", "111111"));
+	    	lp.add(new Phone("Нечаев", "22222"));
+	    	lp.add(new Phone("Васильев", "33333"));
+	    	
+	        model.addAttribute("message", message);
+	        model.addAttribute("phones", lp);
+	 
+	        return "index";
+	    }
+
 		}
